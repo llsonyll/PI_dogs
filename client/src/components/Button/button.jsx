bc54efe29064e1be44fcab41391662ac66abbc22
llsonyll <sony_s07@hotmail.es>
2022-06-20 22:26:21 -0500

    feat: components mocks

---
+++ client/src/components/Button/button.jsx
@@ -0,0 +1,7 @@
+import './button.scss';
+
+const Button = () => {
+    return <div className="button">Button</div>
+}
+
+export default Button;
---
+++ client/src/components/SearchBar/searchBar.jsx
@@ -0,0 +1,7 @@
+import './navBar.scss';
+
+const SearchBar = () => {
+    return <div className="searchBar">SearchBar</div>
+}
+
+export default SearchBar;

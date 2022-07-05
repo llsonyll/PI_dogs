43d5f6f117a42984d3c92f448afbcdf33ec94171
Nice® <sony_s07@hotmail.es>
2022-07-04 19:36:15 -0500

    landing style improvement

--- src/pages/Landing/landing.jsx
+++ src/pages/Landing/landing.jsx
@@ -2,7 +2,6 @@ import "./landing.scss";
 import DogsLogo from "../../assets/dog.png";
 
 // React-Icons
-import { IconContext } from "react-icons";
 import { FaReact, FaNodeJs } from "react-icons/fa";
 import { SiExpress, SiPostgresql } from "react-icons/si";
 
@@ -14,31 +13,27 @@ const Landing = () => {
     <div className="landing">
       <div className="landing__content">
         <div className="landing__content__main">
-          <img src={DogsLogo} alt="dogsLogo" />
+          <img src={DogsLogo} alt="dogsLogo" className="dogsLogo" />
           <div className="title">DOGS API</div>
           <div className="subtitle"> HENRY INDIVIDUAL PROJECT</div>
           <RouteButton text="Iniciar" route="home" />
         </div>
         <div className="landing__content__footer">
           <div className="icons">
-            <IconContext.Provider value={{ color: "black", size: "1.8rem" }}>
-              <SiPostgresql />
-              <SiExpress />
-              <FaReact />
-              <FaNodeJs />
-            </IconContext.Provider>
+            <SiPostgresql />
+            <SiExpress />
+            <FaReact />
+            <FaNodeJs />
           </div>
           <span>
-            {" "}
-            by{" "}
+            by
             <a
               href="https://github.com/llsonyll"
               target="_blank"
               rel="noreferrer"
             >
-              {" "}
-              llsonyll{" "}
-            </a>{" "}
+              llsonyll
+            </a>
           </span>
         </div>
       </div>
